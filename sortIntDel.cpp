d5a1aca268538e1bd2b364570070d8089a14f2d2
Tenshi <69513782+Tenshi-Tries@users.noreply.github.com>
2023-06-05 17:41:59 +0800

    Create sortIntDel.cpp
    activity 2 data structures and algorithms

---
+++ sortIntDel.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int size, arr[100];
+   
+   cout << "Enter array size: "<< endl;
+   cin >> size;
+   
+   int i;
+   for(i = 0; i < size; i++){
+       cout << "Enter " << "element " << (i+1) << ":" <<endl;
+       cin >> arr[i];
+   }
+   int sizeof(arr)/sizeof(arr[0]);
+   sort(arr, arr+n);
+   for(i = 0; i < size; i++){
+       cout << arr[i] << endl;
+      
+   }
+    return 0;
+}
